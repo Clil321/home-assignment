3e5ef330dfd46ea299476fd5c603168e17f21542
Clil Ventura <clilventura@gmail.com>
2021-11-15 13:45:40 +0200

    added breed button

---
+++ src/BreedsButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+function breedButton() {
+  return (
+    <>
+      <Button variant="outline-secondary">Secondary</Button>
+    </>
+  );
+}
+export default breedButton;
